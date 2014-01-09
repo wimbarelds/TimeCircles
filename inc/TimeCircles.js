a4b396af4307399117828d1a7b433a073fa46091
Wim Barelds <wim.barelds@gmail.com>
2014-01-09 16:56:02 +0100

    Use fallback to Date.parse when TimeCircles date format not found, useful for localized timestrings like: "Thu Jan 09 2014 16:55:47 GMT+0100".

--- inc/TimeCircles.js
+++ inc/TimeCircles.js
@@ -77,11 +77,20 @@
     }
     
     function parse_date(str) {
-        var parts = str.split(" ");
-        var date = parts[0].split("-");
-        var time = parts[1].split(":");
-        var d = new Date(date[0], date[1] - 1, date[2], time[0], time[1], time[2]);
-        return d;
+        var match = str.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{1,2}:[0-9]{2}:[0-9]{2}$/);
+        if (match !== null && match.length > 0) {
+            var parts = str.split(" ");
+            var date = parts[0].split("-");
+            var time = parts[1].split(":");
+            return new Date(date[0], date[1] - 1, date[2], time[0], time[1], time[2]);
+        }
+        // Fallback for different date formats
+        var d = Date.parse(str);
+        if(!isNaN(d)) return d;
+        d = Date.parse(str.replace(/-/g, '/').replace('T', ' '));
+        if(!isNaN(d)) return d;
+        // Cant find anything
+        return new Date();
     }
     
     var TC_Instance_List = {};
